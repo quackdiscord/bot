6f31746161a6ba341650566576a6e004e5d46b7f
Kyle Dickey <41174949+dickeyy@users.noreply.github.com>
2024-01-09 00:49:31 -0700

    add small webserver for status page

--- bot.ts
+++ bot.ts
@@ -1,12 +1,12 @@
-import { Client, Collection, Events, GatewayIntentBits, WebhookClient, Options, User } from 'discord.js';
-import registerSlashCommands from './lib/registerSlashCommands';
-import { db } from './lib/db';
-import cmdRun from './lib/cmdRun';
-import { redis } from './lib/redis';
-import { logger } from './lib/logger';
-import config from './config';
-import embedBuilder from './lib/embedBuilder';
-import { addCooldown, isOnCooldown, tellOnCooldown } from './lib/cooldown';
+import { Client, Collection, Events, GatewayIntentBits, WebhookClient, Options, User } from "discord.js";
+import registerSlashCommands from "./lib/registerSlashCommands";
+import { db } from "./lib/db";
+import cmdRun from "./lib/cmdRun";
+import { redis } from "./lib/redis";
+import { logger } from "./lib/logger";
+import config from "./config";
+import embedBuilder from "./lib/embedBuilder";
+import { addCooldown, isOnCooldown, tellOnCooldown } from "./lib/cooldown";
 
 // import event handlers
 // import readyEvent from './events/ready';
@@ -19,12 +19,12 @@ if (env == "dev") {
 }
 
 // process exceptions
-process.on('uncaughtException', (error) => {
+process.on("uncaughtException", (error) => {
     logger.error(error);
 });
 
 // handle timeout errors
-process.on('unhandledRejection', (error) => {
+process.on("unhandledRejection", (error) => {
     logger.error(error);
 });
 
@@ -46,41 +46,41 @@ const intents = [
     GatewayIntentBits.GuildModeration,
     GatewayIntentBits.GuildEmojisAndStickers,
     GatewayIntentBits.GuildInvites,
-    GatewayIntentBits.MessageContent,
-]
+    GatewayIntentBits.MessageContent
+];
 // create the client
-const client:MyClient = new MyClient({
-    intents: intents, 
+const client: MyClient = new MyClient({
+    intents: intents,
     sweepers: {
-		...Options.DefaultSweeperSettings,
-		messages: {
-			interval: 3600, // Every hour...
-			lifetime: 1800,	// Remove messages older than 30 minutes.
-		},
+        ...Options.DefaultSweeperSettings,
+        messages: {
+            interval: 3600, // Every hour...
+            lifetime: 1800 // Remove messages older than 30 minutes.
+        },
         invites: {
             interval: 3600, // Every hour...
-            lifetime: 1800, // Remove invites older than 30 minutes.
-        },
-    },
+            lifetime: 1800 // Remove invites older than 30 minutes.
+        }
+    }
 });
 
 // register the commands
 await registerSlashCommands();
 
 // connect redis
-await redis.connect()
+await redis.connect();
 
 redis.on("error", (error) => {
     logger.error("Redis error: " + error);
-})
+});
 
 // register the webhook client
 const webhookClient = new WebhookClient({ url: config.webhookUrl as string });
 client.webhookClient = webhookClient;
 
 // listen for interaction create events
-client.on(Events.InteractionCreate, async interaction => {
-	if (!interaction.isChatInputCommand()) return
+client.on(Events.InteractionCreate, async (interaction) => {
+    if (!interaction.isChatInputCommand()) return;
 
     // get the command from the collection
     const command = client.commands?.get(interaction.commandName);
@@ -90,7 +90,6 @@ client.on(Events.InteractionCreate, async interaction => {
 
     // try to execute the command
     try {
-        
         try {
             command.execute(interaction);
         } catch (error) {
@@ -98,37 +97,43 @@ client.on(Events.InteractionCreate, async interaction => {
             const embedData = {
                 title: "Error: An error occured when trying to run this command.",
                 description: "Please try again later.",
-                color: "Red",
-            }
-            interaction.reply({ embeds: [ embedBuilder(embedData as any) ], ephemeral: true });
+                color: "Red"
+            };
+            interaction.reply({ embeds: [embedBuilder(embedData as any)], ephemeral: true });
         }
 
         try {
             cmdRun(interaction.commandName, interaction);
         } catch (error) {
             logger.error("Error running cmdRun: " + error);
-        } 
+        }
 
         try {
-            await addCooldown(interaction.user, interaction.commandName, interaction.guild as any, 'oneSec');
+            await addCooldown(interaction.user, interaction.commandName, interaction.guild as any, "oneSec");
         } catch (error) {
             logger.error("Error adding cooldown: " + error);
         }
-        
     } catch (error) {
         logger.error(error);
         const embedData = {
             title: "Error: An error occured when trying to run this command.",
             description: "Please try again later.",
-            color: "Red",
-        }
-        interaction.reply({ embeds: [ embedBuilder(embedData as any) ], ephemeral: true });
+            color: "Red"
+        };
+        interaction.reply({ embeds: [embedBuilder(embedData as any)], ephemeral: true });
     }
 });
 
-
 // export the client
 export { client, db, redis };
+
+// start a small webserver for status page checks
+Bun.serve({
+    port: 8080,
+    fetch(req) {
+        return new Response("hi :)");
+    }
+});
 
 // login with the token
 client.login(config.token);
