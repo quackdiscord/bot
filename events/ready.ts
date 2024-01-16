14bd0d59d89eebca4b65c871e2d6879af0d1240b
dickeyy <kyledickey02@gmail.com>
2024-01-16 02:24:24 -0700

    update events/ready.ts

--- events/ready.ts
+++ events/ready.ts
@@ -4,22 +4,22 @@ import { db } from "../bot";
 import { guilds } from "../schema/guild";
 import DBGuild from "../interfaces/DBGuild";
 
-async function readyEvent(c:Client) {
+async function readyEvent(c: Client) {
     // make sure guilds and db are synced
-    const clientGuilds = c.guilds.cache
-    const dbGuilds = await db.select().from(guilds)
+    const clientGuilds = c.guilds.cache;
+    const dbGuilds = await db.select().from(guilds);
 
     if (clientGuilds.size > dbGuilds.length) {
-        logger.info("DB is missing guilds, adding them now...")
+        logger.info("DB is missing guilds, adding them now...");
 
         // see what guilds are in the client but not in the db
-        const guildsToAdd:Guild[] = Array.from(clientGuilds.values()).filter((guild:Guild) => {
-            return !dbGuilds.find((dbGuild:any) => dbGuild.id == guild.id)
-        })
+        const guildsToAdd: Guild[] = Array.from(clientGuilds.values()).filter((guild: Guild) => {
+            return !dbGuilds.find((dbGuild: any) => dbGuild.id == guild.id);
+        });
 
         // add the guilds to the db
         for (const guild of guildsToAdd.values()) {
-            const data:DBGuild = {
+            const data: DBGuild = {
                 id: guild.id,
                 name: guild.name,
                 description: "",
@@ -40,30 +40,27 @@ async function readyEvent(c:Client) {
                 nsfw_level: guild.nsfwLevel as number,
                 preferred_locale: guild.preferredLocale as string,
                 rules_channel_id: guild.rulesChannelId as string,
-                system_channel_id: guild.systemChannelId as string,
-            }
+                system_channel_id: guild.systemChannelId as string
+            };
             try {
-                await db.insert(guilds).values(data).execute()
-                logger.info(`Added guild ${guild.id} to the db`)
+                await db.insert(guilds).values(data).execute();
+                logger.info(`Added guild ${guild.id} to the db`);
             } catch (err) {
-                logger.error(err)
+                logger.error(err);
             }
         }
-    } 
+    }
 
-    logger.info("Guilds are synced with the db")
+    logger.info("Guilds are synced with the db");
     logger.info(`Logged in as ${c.user?.tag}`);
 
-    c.user?.setActivity('/help | seedsbot.xyz', { type: ActivityType.Watching });
-
+    c.user?.setActivity("/help | seedsbot.xyz", { type: ActivityType.Watching });
 }
 
 const data = {
     name: Events.ClientReady,
     once: true,
-    execute: readyEvent,
-}
+    execute: readyEvent
+};
 
-export { 
-    data,
-}
+export { data };
