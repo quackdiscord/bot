debd15e1c822f391aaf6ecce6270eae997919758
Kyle Dickey <41174949+dickeyy@users.noreply.github.com>
2024-03-29 15:46:18 -0600

    new db

--- lib/db.ts
+++ lib/db.ts
@@ -1,35 +1,13 @@
 // import { MongoClient, ServerApiVersion } from 'mongodb';
-import { logger } from "./logger";
-import { drizzle } from "drizzle-orm/planetscale-serverless";
-import { connect } from "@planetscale/database";
+import { drizzle } from "drizzle-orm/mysql2";
+import mysql from "mysql2/promise";
 import config from "../config";
-import { MongoClient, ServerApiVersion } from "mongodb";
-
-function connectDb() {
-    try {
-        const mClient = new MongoClient(process.env.MONGO_URI as string, {
-            serverApi: ServerApiVersion.v1
-        });
-
-        mClient.connect().then(() => {
-            logger.info("Connected to MongoDB.");
-        });
-
-        return mClient;
-    } catch (error) {
-        logger.error(error);
-    }
-}
-
-// export { connectDb };
 
 // create the connection
-const connection = connect({
-    host: config.database.host,
-    username: config.database.username,
-    password: config.database.password
+const connection = await mysql.createConnection({
+    uri: config.db.uri
 });
 
 const db = drizzle(connection);
 
-export { db, connectDb };
+export { db };
