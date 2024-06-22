90af7298b20df347e6983748d058a8a347f5a8fb
dickeyy <kyledickey02@gmail.com>
2024-06-22 17:24:52 -0600

    add activs

--- events/ready.go
+++ events/ready.go
@@ -1,6 +1,9 @@
 package events
 
 import (
+	"math/rand"
+	"time"
+
 	"github.com/bwmarrin/discordgo"
 	log "github.com/sirupsen/logrus"
 )
@@ -10,5 +13,37 @@ func init() {
 }
 
 func onReady(s *discordgo.Session, r *discordgo.Ready) {
+	// activities
+	actvs := []string{
+		"Making Discord safer.",
+		"Easy moderation for everyone.",
+		"Keeping this server safe.",
+		"Moderation, logging, tickets, & more",
+		"Quackbot.xyz",
+		"Quackbot.xyz/discord",
+		"Quackbot.xyz/invite",
+		"Quackbot.xyz/commands",
+		"Swimming in the pond",
+		"*Duck noises*",
+		"Getting rid of bad eggs",
+		"Formerly Seeds",
+		"Eating bread",
+		"Give me some bread",
+		"Watching you",
+		"Leading the ducklings",
+		"New and Improved!",
+		"Quack v4 out now!",
+	}
+
+	s.UpdateCustomStatus(actvs[rand.Intn(len(actvs))])
+
 	log.Info("Signed in as " + s.State.User.String())
+
+	// every 10 minutes, rotate the activity
+	go func() {
+		for {
+			time.Sleep(10 * time.Minute)
+			s.UpdateCustomStatus(actvs[rand.Intn(len(actvs))])
+		}
+	}()
 }
