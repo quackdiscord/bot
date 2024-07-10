49ef2221acb4fefb87dd3fa09f5c4f9e638ae180
Kyle Dickey <k@kyle.so>
2024-07-10 13:41:27 -0600

    add owner guild cmd

--- events/message_create.go
+++ events/message_create.go
@@ -3,9 +3,11 @@ package events
 import (
 	"fmt"
 	"runtime"
+	"strings"
 	"time"
 
 	"github.com/bwmarrin/discordgo"
+	"github.com/quackdiscord/bot/components"
 	"github.com/quackdiscord/bot/config"
 	"github.com/quackdiscord/bot/log"
 	"github.com/quackdiscord/bot/services"
@@ -32,9 +34,13 @@ func onMessageCreate(s *discordgo.Session, m *discordgo.MessageCreate) {
 
 	if m.Author.ID == config.Bot.BotOwnerID {
 		prefix := "!!!"
-		if m.Content == prefix+"stats" {
+		command := strings.Split(m.Content, " ")[0]
+		if command == prefix+"stats" {
 			statsCommand(s, m)
 			log.Info().Msg("Owner stats command executed")
+		} else if command == prefix+"guild" {
+			guildCommand(s, m)
+			log.Info().Msg("Owner guild command executed")
 		}
 	}
 }
@@ -107,4 +113,58 @@ func statsCommand(s *discordgo.Session, m *discordgo.MessageCreate) {
 		"```", CPUPercent, MemoryUsage, HeapUsed, uptime.Round(time.Second).String(), Servers, msgCacheSize, memberCount, channelCount, roleCount, emojiCount, HeartbeatLatency, RedisPing, DBPing, CmdsRun)
 
 	s.ChannelMessageSend(m.ChannelID, msg)
+}
+
+func guildCommand(s *discordgo.Session, m *discordgo.MessageCreate) {
+	var guild *discordgo.Guild
+	// get the guild id by splitting the message at the space
+	if !strings.Contains(m.Content, " ") {
+		s.ChannelMessageSend(m.ChannelID, "Please provide a guild ID")
+		return
+	}
+
+	guildID := strings.Split(m.Content, " ")[1]
+
+	if guildID == "" {
+		s.ChannelMessageSend(m.ChannelID, "Please provide a guild ID")
+		return
+	}
+
+	// get the guild
+	guild, err := s.Guild(guildID)
+	if err != nil {
+		log.Error().Err(err).Msg("Failed to get guild")
+		s.ChannelMessageSend(m.ChannelID, fmt.Sprintf("Failed to get guild %s ```%s```", guildID, err))
+		return
+	}
+
+	desc := fmt.Sprintf(
+		"**Description:**\n<:text:1229343822337802271>%s\n\n**Numbers**\n<:text2:1229344477131309136>**Members:** %d / %d\n<:text2:1229344477131309136>**Boost Tier:** %d\n<:text:1229343822337802271>**Emojis:** %d\n\n**Owner:**\n<:text:1229343822337802271> <:owner:1230302954683367436> <@%s>",
+		guild.Description, guild.ApproximateMemberCount, guild.MaxMembers, guild.PremiumTier, len(guild.Emojis), guild.OwnerID,
+	)
+
+	if guild.VanityURLCode != "" {
+		desc += "\n\nhttps://discord.gg/" + guild.VanityURLCode
+	} else {
+		inv, err := s.GuildInvites(guild.ID)
+		if err != nil {
+			log.Error().Err(err).Msg("Failed to get guild invites")
+			desc += "\n\nFailed to get guild invites"
+		} else {
+			if len(inv) > 0 {
+				desc += "\n\nhttps://discord.gg/" + inv[0].Code
+			}
+		}
+	}
+
+	embed := components.NewEmbed().
+		SetColor("DarkButNotBlack").
+		SetAuthor(guild.Name, guild.IconURL("")).
+		SetDescription(desc).
+		SetFooter(guild.ID).
+		SetTimestamp().
+		MessageEmbed
+
+	s.ChannelMessageSendEmbed(m.ChannelID, embed)
+
 }
