e5f76c9b52feb2bd1de99acd57e4bbd97f81447d
Kyle Dickey <kyledickey02@gmail.com>
2024-12-20 21:39:57 -0700

    update logging again :)

--- log/log.go
+++ log/log.go
@@ -122,10 +122,6 @@ func init() {
 	}).With().Timestamp().Caller().Logger()
 
 	SetLogCallback(func(level zerolog.Level, msg string, fields map[string]interface{}) {
-		if level == zerolog.InfoLevel && msg != "Command executed" {
-			return
-		}
-
 		// form the log message
 		formatedFields := formatFields(fields)
 		fieldMsg := ""
