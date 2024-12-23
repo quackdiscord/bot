e66f8a39d1d43d88d93e048eba3df718c4320ba0
Kyle Dickey <k@kyle.so>
2024-12-22 18:14:59 -0700

    add unlock command

---
+++ commands/unlock.go
@@ -0,0 +1,89 @@
+package commands
+
+import (
+	"fmt"
+
+	"github.com/bwmarrin/discordgo"
+	"github.com/quackdiscord/bot/components"
+	"github.com/quackdiscord/bot/log"
+	"github.com/quackdiscord/bot/services"
+)
+
+func init() {
+	services.Commands[unlockCmd.Name] = &services.Command{
+		ApplicationCommand: unlockCmd,
+		Handler:            handleUnlock,
+	}
+}
+
+var unlockCmd = &discordgo.ApplicationCommand{
+	Type:                     discordgo.ChatApplicationCommand,
+	Name:                     "unlock",
+	Description:              "Unlock a channel to allow new messages",
+	DefaultMemberPermissions: &moderateMembers,
+	Options: []*discordgo.ApplicationCommandOption{
+		{
+			Type:        discordgo.ApplicationCommandOptionChannel,
+			Name:        "channel",
+			Description: "The channel to unlock",
+			Required:    true,
+		},
+	},
+}
+
+func handleUnlock(s *discordgo.Session, i *discordgo.InteractionCreate) *discordgo.InteractionResponse {
+	c := i.ApplicationCommandData().Options[0].ChannelValue(s)
+	if c == nil {
+		return EmbedResponse(components.ErrorEmbed("Channel not found."), true)
+	}
+
+	if c.Type == discordgo.ChannelTypeGuildText {
+		overwrites := c.PermissionOverwrites
+		// Find the guild role overwrite
+		for i, overwrite := range overwrites {
+			if overwrite.ID == c.GuildID && overwrite.Type == discordgo.PermissionOverwriteTypeRole {
+				// Remove the send message permissions from Deny
+				overwrites[i].Deny &^= discordgo.PermissionSendMessages | discordgo.PermissionSendMessagesInThreads
+
+				// If this overwrite now has no permissions, remove it completely
+				if overwrites[i].Allow == 0 && overwrites[i].Deny == 0 {
+					// Remove this overwrite by taking everything before and after it
+					overwrites = append(overwrites[:i], overwrites[i+1:]...)
+				}
+				break
+			}
+		}
+
+		// Update the channel with modified permission overwrites
+		_, err := s.ChannelEdit(c.ID, &discordgo.ChannelEdit{
+			PermissionOverwrites: overwrites,
+		})
+		if err != nil {
+			log.Error().AnErr("Failed to update channel permissions", err)
+			return EmbedResponse(components.ErrorEmbed("Failed to update channel permissions."), true)
+		}
+
+		// send a message in the channel
+		embed := components.NewEmbed().
+			SetTitle("This channel has been unlocked.").
+			SetDescription("You may now send messages in this channel.").
+			SetColor("Main").
+			SetTimestamp().
+			MessageEmbed
+
+		_, err = s.ChannelMessageSendEmbed(c.ID, embed)
+		if err != nil {
+			return EmbedResponse(components.ErrorEmbed("Failed to send message to channel."), true)
+		}
+
+		embed = components.NewEmbed().
+			SetDescription(fmt.Sprintf("🔓 <#%s> has been unlocked.", c.ID)).
+			SetColor("Main").
+			SetTimestamp().
+			MessageEmbed
+
+		return EmbedResponse(embed, false)
+	}
+
+	return EmbedResponse(components.ErrorEmbed("This channel type is not supported."), true)
+}
