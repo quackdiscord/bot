01157cf3df0e3be10a9ff5c25527688b741c0711
Kyle Dickey <k@kyle.so>
2024-12-29 10:12:36 -0700

    small lil tweak

--- lib/id.go
+++ lib/id.go
@@ -6,11 +6,10 @@ import (
 
 // GenerateID generates a random ID with the default alphabet and length of 8 or an overridden length.
 func GenID(length ...int) (string, error) {
-	alphabet := nanoid.DefaultAlphabet
 	if length == nil {
 		length = append(length, 12)
 	}
-	id, err := nanoid.Generate(alphabet, length[0])
+	id, err := nanoid.Generate(nanoid.DefaultAlphabet, length[0])
 	if err != nil {
 		return "", err
 	}
