ca353848fa11e7429cec3cca781980773b31b71d
Kyle Dickey <k@kyle.so>
2025-01-05 22:28:40 -0700

    fix sigsegv in check-perms

--- utils/check-perms.go
+++ utils/check-perms.go
@@ -4,6 +4,9 @@ import "github.com/bwmarrin/discordgo"
 
 // CheckPerms checks if a user has a certian permission (redundant but just in case discord breaks)
 func CheckPerms(u *discordgo.Member, perms int64) bool {
+	if u == nil {
+		return false
+	}
 	// get the user's guild permissions
 	guildPerms := u.Permissions
 	// check if the user has the required permissions
